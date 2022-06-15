442722573a3c1f74a4104cf0770fa1f312c9b09a
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-06-15 15:09:09 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Main } from './Main/Main';
-
 const HomePage = lazy(() =>
   import('pages/HomePage/HomePage' /* webpackChunkName: "home-view" */)
 );
